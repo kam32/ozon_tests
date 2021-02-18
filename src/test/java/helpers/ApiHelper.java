db650ba55be49971b4ba3f3f757bc7828271368a
kam32 <Cjpr2Cj5c!43Dvf>
2021-02-18 20:32:02 +0300

    fixed

---
+++ src/test/java/helpers/ApiHelper.java
@@ -0,0 +1,20 @@
+package helpers;
+
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+
+public class ApiHelper {
+
+    public Map<String, String> getCookies() {
+        return given()
+                .header("User-Agent", "PostmanRuntime/7.26.10")
+                .header("Connection", "keep-alive")
+                .when()
+                .get("https://www.ozon.ru")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .extract().cookies();
+    }
+}
